c978db70dbacc4088c64073c28c54a00c6762e5c
Devansh Goenka <devanshgoenka97@gmail.com>
2017-01-12 00:29:28 +0530

    removed unnecessary packages

--- config/app.php
+++ config/app.php
@@ -153,7 +153,6 @@ return [
         /*
          * Application Service Providers...
          */
-        PulkitJalan\LaravelMailCssInliner\LaravelMailCssInlinerServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
